2894482262f48a86fd651ad87199ffb44795ec4d
Jacimarct <143716758+Jacimarct@users.noreply.github.com>
2024-05-05 15:10:46 -0300

    meu segundo commit 20240505 15:10

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //TODO: cONHECER E IMPORTAR A CLASSE SCANNER
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        //Exibir as mensagens para o nosso usuário
+        System.out.println("\t\t\t\t\t\t\tSeu Banco Virtual");
+        System.out.println("\t\t\t\t\t\tInforme seus dados para o Cadastro");        
+
+        System.out.println("Informe o nome do cliente ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Informe sua agência");
+        String agencia = scanner.next();
+        
+        System.out.println("Informe sua conta com 4(quatro) digitos ");
+        int conta = scanner.nextInt();
+        String contaFormatada = String.format("%03d-%d", conta / 10, conta % 10);
+
+        System.out.println("Informe Saldo ");
+        double saldo = scanner.nextDouble();
+
+        //Obter pela scanner os valores digitados no terminal
+
+        System.out.println("Cadastrado em nome do Cliente " + nomeCliente );
+        System.out.println("Sua agência é " + agencia + " Sua conta é " + contaFormatada ); //        System.out.println(" - " + contaFormatada);       
+        System.out.println("Seu Saldo é " + saldo);        
     }
 }
